448ba86c0fb8bcd5cdd17478e45553a56dcfd2fc
dev-kitty-ilnazik <ilnazm1ngaleev@yandex.ru>
2025-09-23 19:34:47 +0300

    feat(frontend): add internationalization (i18n) support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { I18nProvider } from '@/providers/I18nProvider.tsx'
 import { QueryProvider } from '@/providers/QueryProvider.tsx'
-import { ThemeProvider } from './providers/ThemeProvider.tsx'
+import { ThemeProvider } from '@/providers/ThemeProvider.tsx'
 
 import App from './App.tsx'
 import './index.css'
@@ -10,9 +11,11 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryProvider>
-			<ThemeProvider>
-				<App />
-			</ThemeProvider>
+			<I18nProvider>
+				<ThemeProvider>
+					<App />
+				</ThemeProvider>
+			</I18nProvider>
 		</QueryProvider>
 	</StrictMode>
 )
